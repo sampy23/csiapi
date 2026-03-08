afdffb8e16caa8599017637870136b92394a37b2
Shahabaz MUHAMMED <Shahabaz.Muhammed@robertbird.com>
2026-03-08 20:05:10 +0400

    made more detailed output

--- processors/get_conccol_force_range.py
+++ processors/get_conccol_force_range.py
@@ -31,22 +31,27 @@ def local():
     df = design_concrete.all_column_design_forces()
     df["P"] = pd.to_numeric(df["P"], errors="coerce") # to force dataframe which are not numeric to number
 
-  # Maximum absolute axial load per column
-    pmax = df.groupby("Unique_Name")["P"].max()
-    pmin = df.groupby("Unique_Name")["P"].min() #critical as this is compression
+    idx = df.groupby("Unique_Name")["P"].idxmax()
 
-    # Global maxima/minima
+    envelope = df.loc[idx][["Unique_Name", "P", "Combo"]]
+    envelope = envelope.set_index("Unique_Name")
+
+    pmax = envelope["P"]
+
     frame_max = pmax.idxmax()
-    frame_min = pmin.idxmin()
+    frame_min = pmax.idxmin()
 
-    p_maxima = pmax.max()
-    p_minima = pmin.min()
+    print("\nControlling axial forces:\n")
+    print(envelope.sort_values("P", ascending=True).head(10))
 
-    print(f"\nMaximum axial load is for {frame_max}, with force of {p_maxima:.2f}kN")
-    print(f"Lowest axial load is for {frame_min}, with force of {p_minima:.2f}kN")
+    p_maxima = float(pmax.max())
+    p_minima = float(pmax.min())
+
+    print(f"\nMaximum axial load: {frame_max} → {p_maxima:.2f} kN")
+    print(f"Minimum axial load: {frame_min} → {p_minima:.2f} kN")
 
     while True:
-        print(f"\nMinima is {p_minima}kN, Maxima is {p_maxima}kN")
+        print(f"\nMinima is {p_minima:.2f}kN, Maxima is {p_maxima:.2f}kN")
         lower_bound = utils.input_float("\nEnter the lower bound of the P value for member selection: ",\
                             p_minima,p_maxima, \
                             reminder="Please enter number which are within the maxima and minima obtained.")
@@ -59,10 +64,10 @@ def local():
         
         # --- SELECT MEMBERS ---
         selected_max = pmax[(pmax >= lower_bound) & (pmax <= upper_bound)]
-        selected_min = pmin[(pmin >= lower_bound) & (pmin <= upper_bound)]
+        # selected_min = pmin[(pmin >= lower_bound) & (pmin <= upper_bound)]
         selected_cols = selected_max.index.tolist()
         selected_cols = list(set(
-            selected_max.index.tolist() + selected_min.index.tolist()
+            selected_max.index.tolist()
         ))
 
         print(f"\nSelecting {len(selected_cols)} columns...")
